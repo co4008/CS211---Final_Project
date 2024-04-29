315f6dbd4f06185b88fcdcb6357aa49aa2f152db
aethersoul2 <167807276+aethersoul2@users.noreply.github.com>
2024-04-29 18:58:16 -0400

    Update Scrimmage.java

--- Scrimmage.java
+++ Scrimmage.java
@@ -1,10 +1,21 @@
+/************************************************************/
+/* Author: Aidan Donohoe, Eric Chen, Chris O'Brien */
+/* Major: Computer Science */
+/* Creation Date: 4/24/2024 */
+/* Due Date: 4/29/2024 */
+/* Course: CS211-02 */
+/* Professor Name: Prof. Shimkanon */
+/* Assignment: Final Project */
+/* Filename: Scrimmage.java */
+/* Purpose: This program will create the event class */
+/************************************************************/
 package application;
 
 import java.util.Date;
-
+//similar to the standard event class, but with the addition of the opponentName variable
 public class Scrimmage extends Event
 {
-	public String opponentName;
+	public String opponentName; //the name of the school, club, or group the team will be scrimmming against 
 	
 	public Scrimmage(Date s, Date e, String g, String n, String d, int r, String o)
 	{
